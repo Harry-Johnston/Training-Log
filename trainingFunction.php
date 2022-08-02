8e22cfc4c21517dda7f06cdc9ae5ad4e1556604a
Harry <virtualslide@gmail.com>
2022-08-02 10:36:31 +0100

    finished function to output database information on a per date basis

--- trainingFunction.php
+++ trainingFunction.php
@@ -18,15 +18,17 @@ function dbPull():array{
     return $allResults;
 }
 
-function outputResults(){}
-
 function perDateOutput(array $db_array, array $date_array){
     foreach($date_array as $date) {
+        echo "<div class='date_container'><h3>$date</h3>";
         foreach ($db_array as $db_item) {
             if ($db_item['date'] === $date) {
-                echo exerciseOutput($db_array, $db_item['id']);
+                $db_id = $db_item['id'] - 1;
+                $exercise_string = exerciseOutput($db_array, $db_id);
+                echo $exercise_string;
             }
         }
+        echo "</div>";
     }
 }
 
@@ -34,33 +36,24 @@ function getAllDates($db_array):array{
     $db_date_array = [];
     foreach($db_array as $db_item){
         $db_date_array[] = $db_item['date'];
-        $date_array = array_unique($db_date_array);
     }
-    return $date_array;
+    return array_unique($db_date_array);
 }
-
 
 function exerciseOutput(array $db_array, int $array_index):string{
     $exercise = $db_array[$array_index]['exercise'];
-    $weight_added_kg = ($db_array[$array_index]['weight_added_kg']===null) ? '' : $db_array[$array_index]['weight_added_kg'];
+    $weight_added_kg = ($db_array[$array_index]['weight_added_kg']===null) ? 0 : $db_array[$array_index]['weight_added_kg'];
     $comments = ($db_array[$array_index]['comments']===null) ? '' : $db_array[$array_index]['comments'];
 
-    $exercise_string = "<div class='exercise_container'><h3> $exercise </h3><p> $weight_added_kg kg</p><p> $comments </p></div>";
+    $exercise_string = "<div class='exercise_container'><h3> $exercise </h3><p> $weight_added_kg kg</p><p>$comments</p></div>";
 
     return $exercise_string;
 }
 
-$db_array = dbPull();
-//echo exerciseOutput($db_array, 1);
-$date_array = getAllDates($db_array);
-echo '<pre>';
-var_dump($date_array);
-
-//$temp_date = $db_array[0]['date'];
-//echo  gettype($temp_date);
+function setupDBOutput():array{
+    $MEGA_ARRAY['db_array'] = dbPull();
+    $MEGA_ARRAY['date_array'] = getAllDates($MEGA_ARRAY['db_array']);
+    return $MEGA_ARRAY;
+}
 
 
-perDateOutput($db_array, $date_array);
-
-echo '<pre>';
-var_dump($db_array);
