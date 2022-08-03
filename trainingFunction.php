e144903d5cd2746774de6e08b6b553965a3dbd59
Harry <virtualslide@gmail.com>
2022-08-03 12:04:40 +0100

    added units tests for all functions

--- trainingFunction.php
+++ trainingFunction.php
@@ -19,6 +19,13 @@ function dbPull():array{
 
 function addHtmlToWorkouts(array $all_workouts_array):array{
     foreach ($all_workouts_array as $key=>$workout_item){
+        if (!is_array($workout_item) ||
+            !array_key_exists('date', $workout_item) ||
+            !array_key_exists('exercise', $workout_item) ||
+            !array_key_exists('weight_added_kg', $workout_item) ||
+            !array_key_exists('comments', $workout_item)) {
+            return [];
+        }
         $exercise = $workout_item['exercise'];
         $weight_added_kg = ($workout_item['weight_added_kg']===null) ? 0 : $workout_item['weight_added_kg'];
         $comments = ($workout_item['comments']===null) ? '' : $workout_item['comments'];
@@ -32,6 +39,11 @@ function perDateOutput(array $all_workouts_array, array $date_array):string{
     foreach($date_array as $date) {
         $per_date_string = "<div class='date_container'><h2 class='date_text'>$date</h2>";
         foreach ($all_workouts_array as $workout_item) {
+            if (!is_array($workout_item) ||
+                !array_key_exists('date', $workout_item) ||
+                !array_key_exists('html', $workout_item)){
+                return '';
+            }
             if ($workout_item['date'] === $date) {
                 $exercise_string = $workout_item['html'];
                 $per_date_string .= $exercise_string;
@@ -46,10 +58,14 @@ function perDateOutput(array $all_workouts_array, array $date_array):string{
     return $output_string;
 }
 
-function getAllDates($all_workouts_array):array{
+function getAllDates(array $all_workouts_array):array{
     $db_date_array = [];
-    foreach($all_workouts_array as $db_item){
-        $db_date_array[] = $db_item['date'];
+    foreach($all_workouts_array as $workout){
+        if (!is_array($workout) ||
+            !array_key_exists('date', $workout)){
+            return [];
+        }
+        $db_date_array[] = $workout['date'];
     }
     return array_unique($db_date_array);
 }
