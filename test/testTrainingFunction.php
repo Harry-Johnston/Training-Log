52cddb368b47a1089b6e383b54b98854dbc352c2
Harry <virtualslide@gmail.com>
2022-08-03 10:33:48 +0100

    fixed un-unit testable code and mdae the search function more efficient

--- test/testTrainingFunction.php
+++ test/testTrainingFunction.php
@@ -2,14 +2,7 @@
 require '../trainingFunction.php';
 use PHPUnit\Framework\TestCase;
 class testTrainingFunction extends TestCase {
-
-    public function testsortDatesSuccess(){
-        $temp_array = [1, 5, 2, 3, 4, 1];
-        $result = sortDates($temp_array);
-        $expected = [5, 4, 3, 2, 1];
-        $this->assertEquals($expected, $result);
-    }
-
+    
     public function testgetAllDatesSuccess_1(){
         $db_array = [['date'=>'2022-07-10', 'exercise'=>'Max Hang','difficulty'=>'hard', 'weight_added_kg'=>50, 'comments'=>'blah blah blah'],
             ['date'=>'2022-04-06', 'exercise'=>'Repeater','difficulty'=>'easy', 'weight_added_kg'=>10, 'comments'=>'these are some words']];
